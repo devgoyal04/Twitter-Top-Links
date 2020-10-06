45d10336373f4faca6c69cf20163fff9b30f67ff
devgoyal04 <goyaldev473@gmail.com>
2020-10-06 20:34:55 +0530

    added frontend

--- topLinks/admin.py
+++ topLinks/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
+from .models import Tweets, Author
 
-# Register your models here.
+admin.site.register(Tweets)
+admin.site.register(Author)
--- topLinks/views.py
+++ topLinks/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponseRedirect, HttpResponse
 from django.urls import reverse
 from django.contrib.auth import logout
+from django.db.models import Count
 
 from datetime import datetime
 
@@ -9,16 +10,20 @@ import requests
 import tweepy
 import tldextract
 
+from .models import Author, Tweets
+
 CONSUMER_KEY = '6Uff1eSm3Filj2q26cFVSehfM'
 CONSUMER_SECRET = '2khEE2qd6pMtdJen5obJgEuQzpb9hgFQM1qNaAAfjWcdjLljuu'
 CALLBACK_URL = 'http://127.0.0.1:8000/callback'
 
+#Landing page view
 def indexView(request):
     if (checkKey(request)):
         return HttpResponseRedirect(reverse('topLinks:home'))
     else:
         return render(request, 'topLinks/index.html', {})
 
+#Authorizing the user
 def oauth(request):
     auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET, CALLBACK_URL)
     auth_url = auth.get_authorization_url()
@@ -27,6 +32,7 @@ def oauth(request):
     request.session['request_token'] = auth.request_token
     return response
 
+#Handling the callback
 def callback(request):
     verifier = request.GET.get('oauth_verifier')
     if verifier is None:
@@ -47,6 +53,7 @@ def callback(request):
 
     return HttpResponseRedirect(reverse('topLinks:home'))
 
+#After the user logged in, the home page view
 def homeView(request):
     if not checkKey(request):
         return HttpResponseRedirect(reverse('topLinks:index'))
@@ -62,22 +69,42 @@ def homeView(request):
     request.session['name'] = api.me().name
 
     tweets = []
+    Tweets.objects.all().delete()
     for status in tweepy.Cursor(api.home_timeline).items(100):
         urls = status.entities['urls']
+        #finding link in a tweet and checking if it is not a retweet link
         if urls and urls[0]['display_url'][0:7] != 'twitter' and checkTweet(status):
             url = 'https://publish.twitter.com/oembed?url=https%3A%2F%2Ftwitter.com%2FInterior%2Fstatus%2F' + status.id_str
             tweets.append(tweetTags(url))
+            
+            users = Author.objects.filter(username = status.user.screen_name)
+            if len(users) == 0:
+                Author(username = status.user.screen_name).save()
+
+            #extracting domain from a link
             info = tldextract.extract(urls[0]['display_url'])
-            print(info.domain)
+            tweet = Tweets(tweet_id = status.id, username = Author.objects.get(username = status.user.screen_name), domain = info.domain)
+            tweet.save()
 
-    return render(request, 'topLinks/home.html', {'tweets': tweets})
+    top_sharers = getTopSharer()
+    top_domains = getTopDomain()
+    print(top_sharers)
+    print(top_domains)
 
+    return render(request, 'topLinks/home.html', {
+        'tweets': tweets,
+        'top_sharers': top_sharers,
+        'top_domains': top_domains
+    })
+
+#Logout the user
 def unauth(request):
     if (checkKey(request)):
         request.session.clear()
         logout(request)
     return HttpResponseRedirect(reverse('topLinks:index'))
 
+#check if users token are still in session or not 
 def checkKey(request):
     try:
         access_key = request.session.get('key', None)
@@ -89,13 +116,40 @@ def checkKey(request):
 
     return True
 
+#get the twitter card from official twitter API
 def tweetTags(url):
     tweetjson = requests.get(url).json()
     tweethtml = tweetjson['html']
     return tweethtml
 
+#Check if tweet is within 7 days time
 def checkTweet(tweet):
     days_old = (datetime.now() - tweet.created_at).days
     if days_old < 8:
         return True
     return False
+
+#Query to find the User that shared maximum domains
+def getTopSharer():
+    query = Tweets.objects.values('username__username').annotate(total=Count('username__username')).order_by('-total')
+    
+    top_sharer = []
+    count = 0
+    for q in query:
+        top_sharer.append(q)
+        count += 1
+        if count == 3:
+            break
+    return top_sharer
+    
+#Query to find the domain that is shared maximum times
+def getTopDomain():
+    query = Tweets.objects.values('domain').annotate(total=Count('domain')).order_by('-total')
+    top_domain = []
+    count = 0
+    for q in query:
+        top_domain.append(q)
+        count += 1
+        if count == 3:
+            break
+    return top_domain
